7be28069426fc04a00abbcee97b5599389ef9a75
Harry Shen <40226717+yrrahnehs@users.noreply.github.com>
2021-08-24 15:20:26 +0700

    Update genjibot.py
    implemented unitbuilds, removed getunits, removeunit. updated upload

--- genjibot.py
+++ genjibot.py
@@ -124,224 +124,253 @@ async def cry(ctx):
 
 @client.command()
 @commands.has_role('rebels')
-async def upload(ctx, *args):
-    if not args:
-        print("NO FILE NAME FOUND")
-        await ctx.send("Missing unit name! Enter a unit name after !upload")
+@client.command()
+async def upload(ctx):
+    units = []
+    namesindb = []
+
+    gearset4 = ["Speed", "Counter", "Attack", "Destruction", "Lifesteal", "Rage", "Revenge", "Injury"]
+    gearset2 = ["Crit", "Hit", "Health", "Defense", "Resist", "Immunity", "Unity", "Penetration"]
+
+    # populate units array
+    with open("unit_info.txt") as file:
+        for line in file:
+            units.append(str(line.split(':')[0]).lower())
+
+    with open("unit_info.txt") as file:
+        for line in file:
+            namesindb.append(str(line.split(':')[0]).title())
+
+    def checkinput(m):
+        return m.channel == ctx.channel and m.author == ctx.author
+
+    stringofnames = ""
+
+    sorted(namesindb)
+    for i in range(len(namesindb)):
+        stringofnames += (str(i+1) + ": " + namesindb[i].title() + "\n")
+
+    await ctx.send(">>> What unit build would you like to upload?")
+    await ctx.send(f"```{stringofnames} ```")
+    # user input unit name
+    unit = await client.wait_for("message", check=checkinput)
+
+    # User picks unit or types it out
+    if unit.content.isdigit():
+        if int(unit.content) <= len(namesindb) and int(unit.content) != 0:
+            unitselection = namesindb[int(unit.content) - 1]
+        else:
+            await ctx.send(">>> Invalid number")
+            return
+    else:
+        if unit.content.title() in gearset4:
+            unitselection = unit.content.title()
+        else:
+            await ctx.send(">>> No unit found")
+            return
+
+    print(unitselection.lower())
+
+    # string for printing out 4-set array and 2-set array
+    listoutgearset4 = ""
+    listoutgearset2 = ""
+    for i in range(len(gearset4)):
+        listoutgearset4 += (str(i + 1) + ": " + gearset4[i] + "\n")
+    for i in range(len(gearset2)):
+        listoutgearset2 += (str(i + 1) + ": " + gearset2[i] + "\n")
+
+    # if user input is in the units
+    if unitselection.lower() in units:
+        found = True
+    else:
+        found = False
+
+    # if its not in units, reply that it is not found
+    if not found:
+        await ctx.send(f"Unit \"{unitselection.title()}\" not found in database")
         return
+    else:
+        # else prompt sets to choose from
+        await ctx.send(">>> Enter the first set: ")
+        await ctx.send(f"```{listoutgearset4}```")
+
+    # user picks out 4-set choice
+    firstsetselection = await client.wait_for("message", check=checkinput)
+
+    # User picks first 4 set choice, either through a number or typing it out
+    if firstsetselection.content.isdigit():
+        if int(firstsetselection.content) <= len(gearset4) and int(firstsetselection.content) != 0:
+            setselection4 = gearset4[int(firstsetselection.content) - 1]
+        else:
+            await ctx.send(">>> Invalid number")
+            return
+    else:
+        if firstsetselection.content.title() in gearset4:
+            setselection4 = firstsetselection.content.title()
+        else:
+            await ctx.send(">>> No set found")
+            return
+
+    await ctx.send(f">>> You have picked " + setselection4)
+    await ctx.send(">>> Enter second set: ")
+    await ctx.send(f"```{listoutgearset2}```")
+    secondsetselection = await client.wait_for("message", check=checkinput)
+    # User picks first 2 set choice, either through a number or typing it out
+    if secondsetselection.content.isdigit():
+        if int(secondsetselection.content) <= len(gearset2) and int(secondsetselection.content) != 0:
+            setselection2 = gearset2[int(secondsetselection.content) - 1]
+        else:
+            await ctx.send(">>> Invalid number")
+            return
+    else:
+        if secondsetselection.content.title() in gearset2:
+            setselection2 = secondsetselection.content.title()
+        else:
+            await ctx.send(">>> No set found")
+            return
+    await ctx.send(">>> You have picked " + setselection4 + " + " + setselection2)
+    await ctx.send(">>> Upload a build image")
+
+    def checkimage(message):
+        attachments = message.attachments
+        if len(attachments) == 0:
+            return False
+        attachment = attachments[0]
+        return attachment.filename.endswith(('.jpg', '.png', 'jpeg'))
+
+    msg = await client.wait_for('message', check=checkimage)
 
     try:
-        url = ctx.message.attachments[0].url
+        image = msg.attachments[0].url
+        await ctx.send('>>> loading . . .')
     except IndexError:
-        print("Error: No attachments")
-        await ctx.send("No image submitted")
-
+        print(">>> Error: No attachments")
+        await ctx.send(">>> No image submitted")
     else:
-        if url[0:26] == "https://cdn.discordapp.com":
-            r = requests.get(url, stream=True)
-            imageName = str(ctx.author.name) + "_" + args[0] + ".jpg"
+        if image[0:26] == "https://cdn.discordapp.com":
+            r = requests.get(image, stream=True)
+            linename = unitselection.title() + ": " + setselection4 + " + " + setselection2
+            tempimagename = unitselection.title() + ":" + setselection4 + "+" + setselection2 + ".jpg"
             with open("unit_links.txt") as file:
                 for line in file:
-                    if line.split(': ')[0] == (str(ctx.author.name) + "_" + args[0]):
-                        await ctx.send("Unit already exists!")
+                    if line.split(';')[0] == linename:
+                        await ctx.send(">>> Unit already exists!")
                         return
 
-            with open(imageName, 'wb') as out_file:
+            with open(tempimagename, 'wb') as out_file:
                 shutil.copyfileobj(r.raw, out_file)
 
-            PATH = imageName
+            PATH = tempimagename
             im = pyimgur.Imgur(CLIENT_ID)
-            uploaded_image = im.upload_image(PATH, title=str(ctx.author.name) + "_" + args[0])
-            os.remove(imageName)
+            uploaded_image = im.upload_image(PATH, title=str(linename))
+            os.remove(tempimagename)
             f = open("unit_links.txt", "a")
-            f.write(str(ctx.author.name).lower() + "_" + args[0].lower() + ": " + uploaded_image.link + "\n")
+            f.write(linename + "; " + uploaded_image.link + "\n")
+            print(uploaded_image.link)
             f.close()
-            await ctx.send("Unit added!")
+            await ctx.send(">>> Unit added! \nCheck with the command !unitbuilds")
 
 
 @client.command()
 @commands.has_role('rebels')
-async def getunits(ctx, *args):
+async def unitbuilds(ctx, *args):
     names = []
-    units = []
+    descriptions = []
+
+    color_dictionary = {"Dark": discord.Color.from_rgb(20, 20, 20),
+                        "Light": discord.Color.from_rgb(234, 234, 9),
+                        "Fire": discord.Color.from_rgb(238, 26, 15),
+                        "Ice": discord.Color.from_rgb(79, 165, 255),
+                        "Earth": discord.Color.from_rgb(6, 209, 8)}
 
     def check(m):
         return m.channel == ctx.channel and m.author == ctx.author
 
     if not args:
-
         # asks user who, then lists out names
-        await ctx.send("Whose units would you like to look at?")
+        await ctx.send(">>> Which unit would you like to look at? \n")
+
+        # adds units to names array
         with open("unit_links.txt") as file:
             for line in file:
-                names.append(line.split('_')[0])
+                names.append(line.split(':')[0])
 
-        # makes it so names are unique
-        for name in set(names):
-            print(name)
-            await ctx.send("- " + name)
+        # makes names unique
+        my_set = list(set(names))
 
-        # user input names
-        user = await client.wait_for("message", check=check)
+        # sorts set
+        my_set = sorted(my_set)
 
-        # checks if user inputted correctly
-        if user.content not in names:
-            await ctx.send(f"User not found!")
-            return
-        else:
-            await ctx.send(f"And what unit would you like to look at?")
+        # prints out names in proper format
+        listedoutnames = ""
+        for i in range(len(my_set)):
+            listedoutnames += (str(i + 1) + " : " + str(my_set[i]) + "\n")
+        await ctx.send(f"```{listedoutnames}```")
 
-        # opens text file and adds unit to units array
-        with open("unit_links.txt") as file:
-            for line in file:
-                if line.split('_')[0] == user.content:
-                    word = line
-                    units.append(word[word.find('_') + 1: word.find(':')])
-
-        # print units from units array
-        await ctx.send(units)
-
+        # user input unit name or number
         unit = await client.wait_for("message", check=check)
 
-        if unit.content not in units:
-            await ctx.send("No unit found!")
-            return
-
-        with open("unit_links.txt") as file:
-            for line in file:
-                if user.content + "_" + unit.content in line:
-                    print(line)
-                    await ctx.send(
-                        f"Hera ya go! Here's " + user.content + "'s " + unit.content + ": " + line.split(': ')[1])
-                    break
-
-    if len(args) == 1 and args[0] == "help":
-        await ctx.send(f"Format is: \"!getunits <name> <unit>\"     OR      \"!getunits\"")
-        return
-
-    if len(args) == 1:
-        with open("unit_links.txt") as file:
-            for line in file:
-                names.append(line.split('_')[0])
-
-        if args[0] not in names:
-            await ctx.send(f"User not found!")
-            return
-        else:
-            await ctx.send(f"And what unit would you like to look at?")
-
-        # opens text file and adds unit to units array
-        with open("unit_links.txt") as file:
-            for line in file:
-                if line.split('_')[0] == args[0]:
-                    word = line
-                    units.append(word[word.find('_') + 1: word.find(':')])
-
-        # print units from units array
-        await ctx.send(units)
-
-        unit = await client.wait_for("message", check=check)
-
-        if unit.content not in units:
-            await ctx.send("No unit found!")
-            return
-
-        with open("unit_links.txt") as file:
-            for line in file:
-                if args[0] + "_" + unit.content in line:
-                    print(line)
-                    await ctx.send(
-                        f"Hera ya go! Here's " + args[0] + "'s " + unit.content + ": " + line.split(': ')[1])
-                    break
-
-    if len(args) == 2:
-        with open("unit_links.txt") as file:
-            for line in file:
-                names.append(line.split('_')[0])
-
-        if args[0] not in names:
-            await ctx.send(f"User not found!")
-            return
-
-            # opens text file and adds unit to units array
-        with open("unit_links.txt") as file:
-            for line in file:
-                if line.split('_')[0] == args[0]:
-                    word = line
-                    units.append(word[word.find('_') + 1: word.find(':')])
-
-        if args[1] not in units:
-            await ctx.send("No unit found!")
-            return
-
-        with open("unit_links.txt") as file:
-            for line in file:
-                if args[0] + "_" + args[1] in line:
-                    print(line)
-                    await ctx.send(
-                        f"Hera ya go! Here's " + args[0] + "'s " + args[1] + ": " + line.split(': ')[1])
-                    break
-
-
-@client.command()
-@commands.has_role('rebels')
-async def removeunit(ctx, *args):
-    def check(m):
-        return m.channel == ctx.channel and m.author == ctx.author
-
-    units = []
-    if not args:
-        await ctx.send("Which unit would you like to remove?")
-        # opens text file and adds unit to units array
-
-        with open("unit_links.txt") as file:
-            for line in file:
-                if line.split('_')[0] == ctx.author.name:
-                    word = line
-                    units.append(word[word.find('_') + 1: word.find(':')])
-
-        # print units from units array
-        await ctx.send(units)
-
-        unit = await client.wait_for("message", check=check)
-
-        imagename = str(ctx.author.name) + "_" + str(unit.content) + ": "
-
-        with open('unit_links.txt') as f:
-            if imagename in f.read():
-                await ctx.send("Unit removed")
-                with open("unit_links.txt", "r") as fi:
-                    lines = fi.readlines()
-                with open("unit_links.txt", "w") as fi:
-                    for line in lines:
-                        if line.split('https:')[0] != imagename:
-                            fi.write(line)
+        if unit.content.isdigit():
+            if int(unit.content) <= len(my_set):
+                userinputtedunit = my_set[int(unit.content) - 1].lower()
+            else:
+                await ctx.send(">>> Invalid choice")
                 return
-        await ctx.send("Unit not found")
-        return
+        else:
+            userinputtedunit = str(unit.content).lower()
 
-    if len(args) == 1 and args[0] == "help":
-        await ctx.send("!removeunit     OR      !removeunit <unit name>")
-        return
+        # scans text file
+        with open("unit_links.txt") as file:
+            for line in file:
+                unitname = line.split(':')[0].lower()
+                # if user input is in the text file
+                if unitname.lower() == userinputtedunit:
+                    # adds the unit set and link to the array
+                    start = ': '
+                    end = ';'
+                    descriptions.append(
+                        (line[line.find(start) + len(start):line.rfind(end)]) + " : " + line.split(';')[1].strip())
 
-    if len(args) == 1:
-        imagename = str(ctx.author.name) + "_" + args[0] + ": "
+        stringofdescriptions = ""
 
-        with open('unit_links.txt') as f:
-            if imagename in f.read():
-                await ctx.send("Unit removed")
-                with open("unit_links.txt", "r") as fi:
-                    lines = fi.readlines()
-                with open("unit_links.txt", "w") as fi:
-                    for line in lines:
-                        if line.split('https:')[0] != imagename:
-                            fi.write(line)
-                return
-        await ctx.send("Unit not found")
-        return
-    else:
-        await ctx.send("Error! Use \"!removeunit help\" for usage")
-        return
+        # turns all description into block of text
+        for i in descriptions:
+            stringofdescriptions += (str(i) + "\n")
+
+        # changes all names in array to lower
+        for i in range(len(names)):
+            names[i] = names[i].lower()
+
+        # checks if user input is in the names-array
+        if unit.content.lower() not in names and not unit.content.isdigit():
+            await ctx.send(">>> No unit found!")
+            return
+        # creates an embed
+        else:
+            embed = discord.Embed(
+                title=userinputtedunit.title(),
+            )
+
+            unitcolor = ""
+            uniturl = ""
+
+            # scans unit-info and gets the unit-thumbnail-url and unit color
+            with open("unit_info.txt") as file:
+                for line in file:
+                    # if unit-info's name is same as user inputted name
+                    if line.split(':')[0].lower() == userinputtedunit:
+                        uniturl = line.split(';')[1].strip()
+                        start = ': '
+                        end = ';'
+                        unitcolor = (color_dictionary[(line[line.find(start) + len(start):line.rfind(end)]).strip()])
+
+            # sets embed description, thumbnail, and color
+            embed.description = f" {stringofdescriptions}\n\n"
+            embed.set_thumbnail(url=uniturl)
+            embed.colour = unitcolor
+
+            await ctx.send(embed=embed)
+
 
 
 client.run(os.getenv('GENJI_TOKEN'))
